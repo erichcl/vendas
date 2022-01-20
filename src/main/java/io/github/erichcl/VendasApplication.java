4d7fba9f5ff0c21eb99a0e24f8d820f2810eca5b
Erich <erichitokiri@gmail.com>
2022-01-20 20:21:37 -0300

    beans

--- src/main/java/io/github/erichcl/VendasApplication.java
+++ src/main/java/io/github/erichcl/VendasApplication.java
@@ -1,5 +1,7 @@
 package io.github.erichcl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
+    @Autowired
+    @Qualifier("applicationName")
+    private String appName;
+
     @GetMapping("/hello")
     public String helloWorld()
     {
-        return "hello world";
+        return appName;
     }
 
     public static void main(String[] args) {
