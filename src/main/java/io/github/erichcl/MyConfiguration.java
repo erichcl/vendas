b2329b5bb842b87db3ff896bbaf8764c773f5c97
Erich <erichitokiri@gmail.com>
2022-01-20 22:20:22 -0300

    Persistindo/Lendo base de dados em memoria

--- src/main/java/io/github/erichcl/MyConfiguration.java
+++ src/main/java/io/github/erichcl/MyConfiguration.java
@@ -1,10 +1,17 @@
 package io.github.erichcl;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Development
 public class MyConfiguration {
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando dev");
+        };
+    }
 
         @Bean(name = "applicationName")
         public String applicationName(){
--- src/main/java/io/github/erichcl/VendasApplication.java
+++ src/main/java/io/github/erichcl/VendasApplication.java
@@ -1,24 +1,35 @@
 package io.github.erichcl;
 
+import io.github.erichcl.domain.entity.Cliente;
+import io.github.erichcl.domain.repositorio.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
-    private String appName;
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+            clientes.salvar(new Cliente("teste1"));
+            clientes.salvar(new Cliente("teste2"));
+            clientes.salvar(new Cliente("teste3"));
+            clientes.salvar(new Cliente("teste4"));
 
-    @GetMapping("/hello")
-    public String helloWorld()
-    {
-        return appName;
+            List<Cliente> todosClientes = clientes.getAll();
+            todosClientes.forEach(System.out::println);
+
+        };
     }
 
     public static void main(String[] args) {
